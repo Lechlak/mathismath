7581ecbd60669d5e6a77b0b32e31db11714d271f
Andrew Lechlak <AndrewLechlak@gmail.com>
2024-11-05 10:34:21 -0500

    Update button.jsx

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -14,7 +14,7 @@ function Button({ text, icon, onClick, className }) {
       }
     >
       <div className="flex items-center justify-center">
-        {icon && <FontAwesomeIcon icon={icon} className={`${text ? "mr-2" : ""}`} />} {/* Use FontAwesomeIcon here */}
+        {icon && <FontAwesomeIcon icon="far fa-check-circle" className={`${text ? "mr-2" : ""}`} />} {/* Use FontAwesomeIcon here */}
         {text}
       </div>
     </button>
