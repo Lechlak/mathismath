2795438ca86c10fec2260dbbc619044a0c44885c
Andrew Lechlak <AndrewLechlak@gmail.com>
2024-11-05 11:18:33 -0500

    Update math-fun-game.jsx

--- src/components/math-fun-game.jsx
+++ src/components/math-fun-game.jsx
@@ -252,8 +252,8 @@ function MathFunGame() {
             alt="Game logo"
             className="h-[200px] mb-8"
           />
-                <FontAwesomeIcon icon={faHome} /> 
-                <FontAwesomeIcon icon={faHome} /> 
+                <FontAwesomeIcon icon="far faHome" /> 
+                <FontAwesomeIcon icon="fas faHome" /> 
           <div className="flex flex-wrap justify-center space-x-8 mb-8">
             {["Addition", "Subtraction", "Multiplication", "Division"].map(
               (operation) => (
