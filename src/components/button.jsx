dde541a4c30dcf9e53ddcfa0b71992a08e55f46e
Andrew Lechlak <AndrewLechlak@gmail.com>
2024-11-07 14:17:08 -0500

    3
    3

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -5,30 +5,24 @@ import * as SolidIcons from '@fortawesome/free-solid-svg-icons';
 import * as RegularIcons from '@fortawesome/free-regular-svg-icons'; 
 
 function Button({ text, icon, imageSrc, onClick, className }) {
-  return (
-    <button
-      onClick={onClick}
-      className={`rounded-full border border-gray-300 py-1 ${
-        num + 1 === 10 || num + 1 === 12 ? "px-3" : "px-4"
-      } ${
-        selectedFactFamily.includes(num + 1)
-          ? "bg-[#fffe8b] border-blue-500 border-2"
-          : ""
-      } ${
-        masteredFamilies.includes(
-          `${selectedOperations.join(",")}-${num + 1}`
-        )
-          ? "bg-green-200"
-          : ""
-      }`}
-    >
-      <div className="flex items-center justify-center">
-        {icon && <FontAwesomeIcon icon={SolidIcons[icon]} className="mr-2" />} 
-        {text && <span className="">{text}</span>}
-        {imageSrc && <img src={imageSrc} alt={text || "Button Image"} className="h-5 w-5" />} {/* Add image support */}
-      </div>
-    </button>
-  );
+// Determine padding based on the value of `text`
+const paddingClass =
+text === 10 || text === 12 ? "py-3 px-4" : "py-4 px-4";
+
+return (
+<button
+  onClick={onClick}
+  className={`text-gray-800 ${paddingClass} rounded-[2px] hover:bg-gray-200 transition-colors duration-200 ${className || ""}`}
+>
+  <div className="flex items-center justify-center">
+    {icon && <FontAwesomeIcon icon={SolidIcons[icon]} className="mr-2" />}
+    {text && <span>{text}</span>}
+    {imageSrc && (
+      <img src={imageSrc} alt={text || "Button Image"} className="h-5 w-5" />
+    )}
+  </div>
+</button>
+);
 }
 
 export default Button;
